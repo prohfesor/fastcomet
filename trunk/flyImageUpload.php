c4bf3c2ddea6c806cdf69c51d17b853f20d0a4a1
prohfesor <prohfesor@03b7454e-6d28-11de-9e5e-bf5a4949f761>
2009-08-17 15:40:34 +0000

    Class "ImageFileUpload" added

---
+++ trunk/flyImageUpload.php
@@ -0,0 +1,29 @@
+<?php 
+
+require_once('flyFileUpload.php');
+require_once('flyImage.php');
+
+class flyImageUpload extends flyFileUpload {
+
+	var $image;
+	
+	
+	function flyImageUpload($pass =null) {
+			flyFileUpload::getInstance();
+	}
+
+	
+	function get( $id ) {
+		$aFile = $this->find($id);
+		if(false === $aFile){
+			return false;
+		}
+		$this->image = new flyImage( $aFile['tmp_name'] );
+		$this->image->filename_original = $aFile['name'];
+		return $this->image;
+	}
+	
+	
+}
+
+?>
