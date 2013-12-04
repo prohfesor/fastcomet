5595da14f6a1fbec83921f2a9dfaa27cfdc8a2bc
prohfesor@gmail.com <prohfesor@gmail.com@03b7454e-6d28-11de-9e5e-bf5a4949f761>
2013-12-04 11:00:51 +0000

    new web app features

--- flyRegistry.php
+++ flyRegistry.php
@@ -2,5 +2,14 @@
 
 class flyRegistry
 {
+    static $registry =array();
+
+    public static function get($key, $default =false){
+        return (isset(self::$registry[$key])) ? self::$registry[$key] : $default ;
+    }
+
+    public static function set($key, $value){
+        self::$registry[$key] = $value;
+    }
 
 }
