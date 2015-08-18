46c6841721254c2c89f945774af9332e2e0ea87f
Pavel Dvoinos <prohfesor@gmail.com>
2015-08-18 18:14:36 +0300

    added phpdoc

--- flyRegistry.php
+++ flyRegistry.php
@@ -1,13 +1,30 @@
 <?php
 
+/**
+ * Class flyRegistry
+ * Store for global variables.
+ *
+ */
 class flyRegistry
 {
     static $registry =array();
 
+    /**
+     * Get variable from registry
+     * @param $key
+     * @param bool|false $default
+     * @return bool
+     */
     public static function get($key, $default =false){
         return (isset(self::$registry[$key])) ? self::$registry[$key] : $default ;
     }
 
+
+    /**
+     * Put variable to registry
+     * @param $key
+     * @param $value
+     */
     public static function set($key, $value){
         self::$registry[$key] = $value;
     }
