598a20e4ada6d0c1d54c15a7e0463ba9f535982e
Pavel Dvoinos <prohfesor@gmail.com>
2015-09-01 23:53:29 +0300

    empty function prototypes

---
+++ src/flyDbOrm.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * author: @prohfesor
+ * package: frm.local
+ * Date: 01.09.2015
+ * Time: 23:48
+ */
+class flyDbOrm
+{
+
+    public function get() {}
+    public function getFirst() {}
+    public function getAll() {}
+    public function findBy() {}
+    public function findOneBy() {}
+    public function set() {}
+    public function save() {}
+
+}
